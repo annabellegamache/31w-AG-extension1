ac94796e10e51aee0d25cc0fff931a8a99616c8a
Annabellle <annabellegamache@gmail.com>
2022-06-02 11:40:21 -0400

    s3-c2 configuration de notre permier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,9 @@
+<?php
+/** 
+*Plugin Name: Exercie 1 mon première extension
+*Description: Première extension pour exercice 1
+*Author: Annabelle Gamache
+*Author URI : https://github.com/annabellegamache/31w-AG.git
+*Version: 1.7.2
+*/
+?>
